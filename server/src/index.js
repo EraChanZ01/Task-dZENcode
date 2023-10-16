3c8a8735d2bb9afddf0114cae2a4adf521fee318
EraChanZ01 <ilya73960@gmail.com>
2023-10-16 15:38:59 +0300

    init

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = require('./router/router')
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/", router)
+
+app.listen(3000, () => {
+    console.log('Application listening on port 3333!');
+})
